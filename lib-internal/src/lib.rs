a083c020c7f1107e6885c8c34683f30cc0bc2ade
SpiveeWorks <spiveehere@gmail.com>
2017-11-19 09:25:32 +1100

    player outline
    need to change sulphate a little to make it work

--- lib-internal/src/lib.rs
+++ lib-internal/src/lib.rs
@@ -2,4 +2,15 @@ extern crate sulphate_lib;
 extern crate lib_math;
 
 pub mod physics;
+pub mod player;
 
+pub mod sulphate {
+    use sulphate_lib::entity_heap;
+    use sulphate_lib::event_queue;
+
+    use physics;
+
+    pub type EventQueue = event_queue::EventQueue<physics::Time>;
+    pub type EntityHeap = entity_heap::EntityHeap;
+    pub type EntityId = entity_heap::UID;
+}
---
+++ lib-internal/src/player.rs
@@ -0,0 +1,61 @@
+use std::sync::mpsc;
+
+use sulphate;
+use physics;
+
+pub struct Player {
+    body: physics::Body,
+    // stimulus from the game world
+    update: mpsc::Sender<Update>,
+}
+
+
+pub enum Control {
+    Move {
+        velocity: physics::Velocity
+    }
+}
+
+pub struct Update {
+    pub when: physics::Time,
+    pub what: UpdateData,
+}
+
+pub enum UpdateData {
+    Created {
+        location: physics::Position,
+    }
+}
+
+pub trait External {
+    fn update(
+        space: &mut sulphate::EntityHeap,
+        time: &mut sulphate::EventQueue,
+        id: sulphate::EntityId,
+        data: Control,
+    ) {
+        use self::Control::*;
+        match data {
+            Move { velocity } => {
+                if let Some(ent) = space.get_mut(id) {
+                    let player: &mut Player = ent;
+                    player.body.bounce(velocity, time.now());
+                }
+            }
+        }
+    }
+}
+
+impl Player {
+    pub fn new(
+        space: &mut sulphate::EntityHeap,
+        time: &mut sulphate::EventQueue,
+        position: physics::Position,
+        update: mpsc::Sender<Update>,
+    ) -> sulphate::EntityId {
+        let body = physics::Body::new_frozen(position);
+        let player = Player { body, update };
+        // TODO implement fn add(self: &mut Self, val: T) -> EntityId
+        space.add(player)
+    }
+}
