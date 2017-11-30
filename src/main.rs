00ffcbcc98f48046720c1f328dcd1dcdd82c9a5f
SpiveeWorks <spiveehere@gmail.com>
2017-12-01 07:56:04 +1100

    Build window on non-sRGB systems took the fix from PistonDevelopers/piston-examples#336 not sure why this isn't default behaviour now for the runtime errors :)

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,37 @@ extern crate city_internal;
 mod client;
 
 
-fn settings() -> WindowSettings {
-    WindowSettings::new(
-        "charm-game",
-        [600, 600]
-    ).exit_on_esc(true)
+// as seen in https://github.com/PistonDevelopers/piston-examples/issues/336
+fn build_window() -> PistonWindow {
+    let title = "lil-city";
+    let resolution = [600, 600];
+    let opengl = OpenGL::V3_2;
+    let mut window_result = WindowSettings::new(title, resolution)
+        .exit_on_esc(true)
+        .srgb(true)                      // try to init windowbuilder with srgb enabled
+        .opengl(opengl)
+        .build();
+    if window_result.is_err() {   //if srgb=true fails, retry as srgb=false
+        window_result = WindowSettings::new(title, resolution)
+            .exit_on_esc(true)
+            .srgb(false)                 // !!!
+            .opengl(opengl)
+            .build();
+    }
+    let window_inner = window_result
+        .unwrap_or_else(|e| {
+            panic!("Failed to build PistonWindow: {}", e);
+        });
+
+    PistonWindow::new(opengl, 0, window_inner)
 }
+
 
 fn main() {
     let mut client = client::start_game();
 
-    let mut window: PistonWindow =
-        settings().build()
-                  .unwrap_or_else(|e| {
-                      panic!("Failed to build PistonWindow: {}", e)
-                  });
+    let mut window: PistonWindow = build_window();
+
     while let Some(e) = window.next() {
         if let Some(ren) = e.render_args() {
             window.draw_2d(&e, |c, g| client.on_draw(c, g, ren));
