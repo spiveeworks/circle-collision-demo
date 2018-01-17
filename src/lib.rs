162589c6d7fcc90beb7f6436fd6f0acbecfeaf77
SpiveeWorks <spiveehere@gmail.com>
2018-01-17 11:05:05 +1100

    blank app using my new piston_app repo

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+extern crate piston_app;
+extern crate piston_window;
+
+use piston_window::*;
+
+pub struct App {
+}
+
+impl piston_app::App for App {
+    fn window_name() -> &'static str {
+        "inductive programming"
+    }
+
+    fn window_starting_size() -> [u32; 2] {
+        [600, 600]
+    }
+
+    fn on_draw(
+        self: &mut Self,
+        _centre: Context,
+        _graphics: &mut G2d,
+        _args: RenderArgs,
+    ) {
+    }
+
+    fn on_update(
+        self: &mut Self,
+        _args: UpdateArgs,
+    ) {
+    }
+
+    fn on_input(
+        self: &mut Self,
+        _args: ButtonArgs,
+    ) {
+    }
+
+    fn on_mouse_move(
+        self: &mut Self,
+        _mouse: [f64; 2],
+    ) {
+    }
+}
+
+pub fn start_app() -> App {
+    App {}
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate piston_app;
+
+extern crate induction;
+
 fn main() {
-    println!("Hello, world!");
+    let app = induction::start_app();
+
+    piston_app::run_until_escape(app);
 }
